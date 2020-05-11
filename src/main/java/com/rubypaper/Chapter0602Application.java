287b4ef96a0659292b78cf25accd6cb9513ed26b
iksrok <iksrok@mqnic.com>
2020-05-11 15:46:42 +0900

    Initial commit

---
+++ src/main/java/com/rubypaper/Chapter0602Application.java
@@ -0,0 +1,13 @@
+package com.rubypaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter0602Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter0602Application.class, args);
+    }
+
+}
